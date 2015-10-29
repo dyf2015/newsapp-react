d7aec3044de7aa1a0d5c14aae4aa87fd1c3a79c3
dYb <duanyubin2012@gmail.com>
2015-10-29 11:30:57 +0800

    Add examples folder, needed to complete

---
+++ examples/webpack.config.js
@@ -0,0 +1,33 @@
+var fs = require('fs')
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+
+  devtool: 'inline-source-map',
+
+  entry: fs.readdirSync(__dirname).reduce(function (entries, dir) {
+    if (fs.statSync(path.join(__dirname, dir)).isDirectory())
+      entries[dir] = path.join(__dirname, dir, 'app.js')
+
+    return entries
+  }, {}),
+
+  output: {
+    path: __dirname + '/__build__',
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
+
+}
