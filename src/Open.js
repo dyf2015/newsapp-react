026ba916cc13bef6ebc840fa7a1d92fd55cb8f4d
dYb <duanyubin2012@gmail.com>
2015-12-28 14:42:12 +0800

    Now open componnet support pages outside of newsapp

--- src/Open.js
+++ src/Open.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Pubsub from 'ntes-pubsub';
+import platform from './platform';
 
 export default class Open extends React.Component {
   constructor(props) {
@@ -9,20 +10,44 @@ export default class Open extends React.Component {
     this.token = Pubsub.subscribe('newsapp:open', (param)=>{
       let iframe = this.refs.iframe
       if(/[A-Z0-9]{16}/.test(param)){
-        iframe.src = 'newsapp://docid/' + param //文章
+        if(platform.isNewsapp){
+          iframe.src = 'newsapp://doc/' + param //文章  
+        }else{
+          window.location.href = 'http://3g.163.com/ntes/special/0034073A/wechat_article.html?docid=' + param
+        }
       }else if(/^S[0-9]{13}/.test(param)){
-        iframe.src = 'newsapp://topic/' + param //专题
+        if(platform.isNewsapp){
+          iframe.src = 'newsapp://topic/' + param //专题
+        }else{
+          window.location.href = 'http://3g.163.com/ntes/special/00340EPA/wapSpecialModule.html?sid=' + param
+        }
       }else if(/^http/.test(param)){
-        iframe.src = 'newsapp://web/' + param // 网页
+        if(platform.isNewsapp){
+          iframe.src = 'newsapp://web/' + param // 网页
+        }else{
+          window.location.href = param
+        }
       }else if(/^T[0-9]{13}/.test(param)){
         iframe.src = 'newsapp://reader/' + param // 订阅
+      }else if(+param){
+        if(platform.isNewsapp){
+          iframe.src = 'newsapp://live/' + param //直播
+        }else{
+          window.location.href = 'http://3g.163.com/ntes/special/00340BF8/seventlive.html?spsw=1&spsf=wx&spss=newsapp&roomid=' + param
+        }
+      }else if(/^V/.test(param)){
+        if(platform.isNewsapp){
+          iframe.src = 'newsapp://video/' + param //视频
+        }else{
+          window.location.href = 'http://3g.163.com/ntes/special/0034073A/wechat_article.html?videoid=' + param
+        }
       }else if(/^[A-Z0-9]{8}|[0-9]{0,9}/.test(param)){
         let temp = param.split('|')
-        iframe.src = 'newsapp://photo/' + temp[0].slice(-4) + '/' + temp[1] // 图集
-      }else if(+param){
-        iframe.src = 'newsapp://live/' + param //直播
-      }else if(/^V/.test(param)){
-        iframe.src = 'newsapp://video/' + param //视频
+        if(platform.isNewsapp){
+          iframe.src = 'newsapp://photo/' + temp[0].slice(-4) + '/' + temp[1] // 图集
+        }else{
+          window.location.href = `http://3g.163.com/ntes/special/0034073A/photoshare.html?setid=${temp[1]}&channelid=${temp[0].slice(-4)}`
+        }
       }
     })
   }
